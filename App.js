d6fdf0e6cffb2536760cee9c81707f4fe0b5970b
Alaap008 <alaapbanerjee08@gmail.com>
2025-02-26 18:44:00 +0530

    Initial Setup

---
+++ App.js
@@ -0,0 +1,3 @@
+const heading  = React.createElement("div",{id : "parent"}, React.createElement("div",{id : "child"},[React.createElement("h1",{},"H1 Tag"), React.createElement("h2",{},"H2 Tag")]));
+const root= ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);

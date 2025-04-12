0e8273c97265eb03678808bd2054e51d3564fbab
Alaap Banerjee <alaapbanerjee@Alaaps-MacBook-Air.local>
2025-04-12 11:27:18 +0530

    Added restaurant cards

--- App.js
+++
@@ -1,3 +0,0 @@
-const heading  = React.createElement("div",{id : "parent"}, React.createElement("div",{id : "child"},[React.createElement("h1",{},"H1 Tag"), React.createElement("h2",{},"H2 Tag")]));
-const root= ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
---
+++ src/components/Body.js
@@ -0,0 +1,19 @@
+import resList from "../utils/mockData";
+import RestaurantCard from "./RestaurantCard";
+
+const Body = () =>{
+    return (
+        <div className="body">
+        <div className="search">
+                Search
+        </div>
+        <div className="res-container">
+        {resList.map((res) => (
+            <RestaurantCard key={res.data.id} resData={res} />
+        ))}
+        </div>
+        </div> 
+    ) 
+}
+
+export default Body;

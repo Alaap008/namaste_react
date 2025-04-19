a6dc3f87758904dbf56cf2d6faa139214f839460
Alaap Banerjee <alaapbanerjee@Alaaps-MacBook-Air.local>
2025-04-19 19:47:16 +0530

    Added new routes

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,19 +1,62 @@
+import { useEffect, useState } from "react";
 import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
-
+import Shimmer from "./Shimmer";
+//Comment
 const Body = () =>{
-    return (
+
+    const [listOfRestaurants, setlistOfRestaurants] = useState([]);
+    const [filterList, setFilterList] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    useEffect(()=>{
+        fetchData();
+    },[])
+    const fetchData  = async () =>{
+        let data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.52110&lng=73.85020&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        data= await data.json();
+        setlistOfRestaurants(
+            data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+          );
+          setFilterList(
+            data?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+          );     
+    }
+    return listOfRestaurants.length === 0 ? (
+        <Shimmer />
+      ) : (
         <div className="body">
         <div className="search">
+            <input type="text" 
+                className="search"
+                value={searchTerm}
+                onChange={(e)=>{
+                    setSearchTerm(e.target.value);
+                }}
+            />
+                <button onClick={() =>{
+                    console.log("Button Clicked");
+                    setFilterList(listOfRestaurants.filter((res) => res.info.name.toLowerCase().includes(searchTerm)));
+
+                }}>
                 Search
+                </button> 
         </div>
+        <button className="filter-btn" onClick={() =>{
+            setlistOfRestaurants( resList.filter(res => (res.data.avgRating) > 4))
+            
+        }} >
+        Top Rated Restaurants
+        </button>
         <div className="res-container">
-        {resList.map((res) => (
-            <RestaurantCard key={res.data.id} resData={res} />
+        {filterList.map((res) => (
+            <RestaurantCard key={res.info.id} resData={res.info} />
         ))}
         </div>
         </div> 
     ) 
 }
+
+
 
 export default Body;
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { CDN_URL } from "../utils/constants";
 const RestaurantCard = (props) => {
     const { resData } = props;
@@ -7,10 +8,11 @@ const RestaurantCard = (props) => {
         avgRating,
         cuisines,
         costForTwo,
-        deliveryTime,
-    } = resData?.data || {};
-
+        sla,
+    } = resData || {};
+    const restaurantId = resData.id?.toString();
     return (
+    <Link to={"/restaurant/" + restaurantId}>
     <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
       <img
         className="res-logo"
@@ -20,11 +22,10 @@ const RestaurantCard = (props) => {
       <h3>{name}</h3>
       <h4>{cuisines.join(", ")}</h4>
       <h4>{avgRating} stars</h4>
-      <h4>₹{costForTwo / 100} FOR TWO</h4>
-      <h4>{deliveryTime} minutes</h4>
+      <h4>{costForTwo}</h4>
+      <h4>{sla.deliveryTime} minutes</h4>
     </div>
-
-
+    </Link>
     )
 }
 
